251abf1c8a9ddf57f7b4faa2426c6a681b4d6aaa
Ethan Etomi <ethanetomi@icloud.com>
2023-11-05 02:11:17 -0500

    Hello and Goodbye

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(args[0]);
+        Scanner sc2 = new Scanner(args[1]);
+        System.out.println("Hello"+ sc1 +" " + "Hello" + sc2);
+        System.out.println("Goodbye"+ sc1 +" " + "Goodbye" + sc2);
+
+    }
+}
